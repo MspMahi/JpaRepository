897bce5e11f71c195dfa98573c1f65264f113c48
Mahi <Mahi@Acer>
2024-03-14 18:59:19 +0530

    dao created by dev

---
+++ SpringBootJpa/src/main/java/com/nit/serive/ISerice.java
@@ -0,0 +1,9 @@
+package com.nit.serive;
+
+import com.nit.entity.Employe;
+
+public interface ISerice {
+	
+	public Employe getbyId(int id);
+
+}
---
+++ SpringBootJpa/src/main/java/com/nit/serive/Serviceimp.java
@@ -0,0 +1,13 @@
+package com.nit.serive;
+
+import com.nit.entity.Employe;
+
+public class Serviceimp implements ISerice {
+
+	@Override
+	public Employe getbyId(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
